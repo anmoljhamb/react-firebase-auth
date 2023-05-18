079a9238cafa3d55c47e10f87e20d214aed8b1bd
Anmol Jhamb <talktoanmol@outlook.com>
2023-05-18 16:52:21 +0530

    Navlinks to login and sign up added to the landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Landing = () => {
-    return <div>Landing</div>;
+    return (
+        <div>
+            <h1>Landing Page </h1>
+            <hr />
+            <NavLink to={"/login"}>Login Page</NavLink>
+            <NavLink to={"/signup"}>Sign Up Page</NavLink>
+        </div>
+    );
 };
